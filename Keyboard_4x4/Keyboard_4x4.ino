62a46f4334586434e2fb26c48575f0024ac134b1
rm1992rm <rei.m92@gmail.com>
2020-08-12 23:37:42 +0700

    Change Hotkey to fix Key Repeated

--- Keyboard_4x4/Keyboard_4x4.ino
+++ Keyboard_4x4/Keyboard_4x4.ino
@@ -24,13 +24,14 @@ void setup(){
   Keyboard.begin();
 }
 
-void sendMacroCommand(uint8_t key) {
+void sendMacroNum(uint8_t key) {
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(key);
+}
+void sendMacroChar(uint8_t key) {
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press(key);
-  Keyboard.releaseAll(); // this releases the buttons
-
 }
 
   
@@ -39,64 +40,53 @@ void loop(){
 
   switch(customKey){
     case '1':
-      Keyboard.press(KEY_F13);
-      Keyboard.release(KEY_F13);
+      sendMacroNum(KEY_F13);
       break;
     case '2':
-      Keyboard.press(KEY_F14);
-      Keyboard.release(KEY_F14);
+      sendMacroNum(KEY_F14);
       break;
     case '3':
-      Keyboard.press(KEY_F15);
-      Keyboard.release(KEY_F15);
+      sendMacroNum(KEY_F15);
       break;
     case '4':
-      Keyboard.press(KEY_F16);
-      Keyboard.release(KEY_F16);
+      sendMacroNum(KEY_F16);
       break;
     case '5':
-      Keyboard.press(KEY_F17);
-      Keyboard.release(KEY_F17);
+      sendMacroNum(KEY_F17);
       break;
     case '6':
-      Keyboard.press(KEY_F18);
-      Keyboard.release(KEY_F18);
+      sendMacroNum(KEY_F18);
       break;
     case '7':
-      Keyboard.press(KEY_F19);
-      Keyboard.release(KEY_F19);
+      sendMacroNum(KEY_F19);
       break;
     case '8':
-      Keyboard.press(KEY_F20);
-      Keyboard.release(KEY_F20);
+      sendMacroNum(KEY_F20);
       break;
     case '9':
-      Keyboard.press(KEY_F21);
-      Keyboard.release(KEY_F21);
+      sendMacroNum(KEY_F21);
       break;
     case '*':
-      Keyboard.press(KEY_F22);
-      Keyboard.release(KEY_F22);
+      sendMacroNum(KEY_F22);
       break;
     case '0':
-      Keyboard.press(KEY_F23);
-      Keyboard.release(KEY_F23);
+      sendMacroNum(KEY_F23);
       break;
     case '#':
-      Keyboard.press(KEY_F24);
-      Keyboard.release(KEY_F24);
+      sendMacroNum(KEY_F24);
       break;
     case 'A':
-      sendMacroCommand(KEY_F13);
+      sendMacroChar(KEY_F13);
       break;
     case 'B':
-      sendMacroCommand(KEY_F14);
+      sendMacroChar(KEY_F14);
       break;
     case 'C':
-      sendMacroCommand(KEY_F15);
+      sendMacroChar(KEY_F15);
       break;
     case 'D':
-      sendMacroCommand(KEY_F16);
+      sendMacroChar(KEY_F16);
       break;
   }
+  Keyboard.releaseAll(); // this releases the buttons
 }
